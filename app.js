503e7d8a26db66ea112b4f810aa0b67b4b776c11
yuchan0701 <83865986+yuchan0701@users.noreply.github.com>
2021-09-24 14:48:37 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+// JavaScript source code
+document.getElementById("pt").style.display ="none";
+var mato1 = document.getElementById("mato");
+mato1.style.position = "absolute";
+
+var point = 0;
+
+
+//ここだよをクリックすると　こうなる
+function touch(){
+
+
+
+var ranX = Math.round( Math.random()*1000 )
+var ranY = Math.round( Math.random()*800 )
+
+mato1.style.left = ranX + "px";
+mato1.style.top = ranY + "px";
+
+point ++;
+
+
+}
+
+//時間制限
+let count = 10;
+  const countUp = () => {
+    console.log(count--);
+    document.getElementById("time").textContent = count;
+
+    if (count == -1){
+        document.getElementById("mato").style.display ="none";
+        document.getElementById("time").style.display ="none";
+        document.getElementById("pt").textContent = "result:"+point + " click";
+        document.getElementById("pt").style.display ="block";
+
+    }
+
+  }
+  setInterval(countUp, 1000);
+
+
