dc41bacc126f6a1f6f250955157a05ecb718687a
yuchan0701 <83865986+yuchan0701@users.noreply.github.com>
2022-11-16 09:01:30 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,43 +2,37 @@
 document.getElementById("pt").style.display ="none";
 var mato1 = document.getElementById("mato");
 mato1.style.position = "absolute";
-
 var point = 0;
-
 
 //ここだよをクリックすると　こうなる
 function touch(){
 
+  var ranX = Math.round( Math.random()*(window.innerWidth-100) )
+  var ranY = Math.round( Math.random()*(window.innerHeight-100) )
 
+  mato1.style.left = ranX + "px";
+  mato1.style.top = ranY+ "px";
 
-var ranX = Math.round( Math.random()*(window.innerWidth) )
-var ranY = Math.round( Math.random()*(window.innerHeight) )
-
-console.log(window.innerHeight,window.innerWidth);
-
-mato1.style.left = ranX + "px";
-mato1.style.top = ranY + "px";
-
-point ++;
-
+  point ++;
 
 }
 
 //時間制限
 let count = 10;
-  const countUp = () => {
-    console.log(count--);
-    document.getElementById("time").textContent = count;
+const countUp = () => {
+  console.log(count--);
+  console.log(window.innerHeight);
+  document.getElementById("time").textContent = count;
 
-    if (count == -1){
-        document.getElementById("mato").style.display ="none";
-        document.getElementById("time").style.display ="none";
-        document.getElementById("pt").textContent = "result:"+point + " click";
-        document.getElementById("pt").style.display ="block";
-
-    }
+  if (count == -1){
+    document.getElementById("mato").style.display ="none";
+    document.getElementById("time").style.display ="none";
+    document.getElementById("pt").textContent = "result:"+point + " click";
+    document.getElementById("pt").style.display ="block";
 
   }
-  setInterval(countUp, 1000);
+}
+  
+setInterval(countUp, 1000);
 
 
